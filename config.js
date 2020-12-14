d0d17db2c4e59537bd2078e5300a349301c090be
studentfordemo <64936561+studentfordemo@users.noreply.github.com>
2020-12-15 04:55:32 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -2,14 +2,14 @@ import firebase from 'firebase';
 require('@firebase/firestore')
 
 var firebaseConfig = {
-  apiKey: "AIzaSyCD7Rcu9UZ5_mo122jpMObZ7NEDyM-YFaA",
-  authDomain: "booksanta-e65b6.firebaseapp.com",
-  databaseURL: "https://booksanta-e65b6.firebaseio.com",
-  projectId: "booksanta-e65b6",
-  storageBucket: "booksanta-e65b6.appspot.com",
-  messagingSenderId: "880954806855",
-  appId: "1:880954806855:web:3fb4bb74ee28b229081762",
-  measurementId: "G-MVR4FWDSML"
+  apiKey: "AIzaSyB-5enU8jzvQP8wWMiOgsosvUrU-By6XkM",
+    authDomain: "check-64767.firebaseapp.com",
+    databaseURL: "https://check-64767.firebaseio.com",
+    projectId: "check-64767",
+    storageBucket: "check-64767.appspot.com",
+    messagingSenderId: "93906471895",
+    appId: "1:93906471895:web:e7cb88b59c8b8c0db2d992",
+    measurementId: "G-0SHXN7MWGF"
   };
 
 
